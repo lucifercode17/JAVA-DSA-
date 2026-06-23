a59587c40f8cb9528ddea3784c6d6f5f6f7a3368
lucifercode17 <jareehd121@gmail.com>
2026-06-23 10:59:40 +0530

    array question and logic

---
+++ ARRAY/multidensionalarray.java
@@ -0,0 +1,21 @@
+package ARRAY;
+
+public class multidensionalarray {
+  public static void main(String[] args) {
+    // the array in store in such format 
+
+    /* 
+        1 2 3 .
+        4 5 6 .
+        . . . .
+
+     */
+
+        // it can be make using this syntax 
+        //  int[][] arr = new int[i][j] where i should be filling is comparsory 
+
+        int[][] arr = new int[4][5];
+        
+  }
+  
+}
