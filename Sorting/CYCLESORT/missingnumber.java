08536d864c847184c545afa061afc19df33d81e9
lucifercode17 <jareehd121@gmail.com>
2026-07-23 05:05:57 +0530

    finding muissing element in sort array

---
+++ Sorting/CYCLESORT/missingnumber.java
@@ -0,0 +1,42 @@
+package Sorting.CYCLESORT;
+
+public class missingnumber {
+  public static void main(String[] args) {
+    int[] arr = {4,0,2,1};
+    int ans = missingNumber(arr);
+    System.out.println(ans);
+    
+  }
+  // the range  is [0,n]
+  static int missingNumber(int[] arr){
+    int index =0;
+    
+    while(index < arr.length){
+      int correct = arr[index];
+      if( arr[index]< arr.length && correct != index){
+        int temp = arr[index];
+        arr[index] = arr[correct];
+        arr[correct] = temp;
+
+
+      }else{
+        index++;
+      }
+
+
+
+    }
+    // search the missing number 
+    for (int i = 0; i < arr.length; i++) {
+      if(arr[i] != i){
+        return i;
+      }
+
+      
+    }
+    return arr.length;
+  
+
+  }
+  
+}
