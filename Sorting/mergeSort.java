8c71269259bf8f8e087e3dd34752908677f76005
lucifercode17 <jareehd121@gmail.com>
2026-08-16 11:03:18 +0530

    merage sort

--- Sorting/mergeSort.java
+++ Sorting/mergeSort.java
@@ -1,3 +1,62 @@
+package Sorting;
+
+import java.util.Arrays;
+
+
+
 public class mergeSort {
-  
+  public static void main(String[] args) {
+    int[] arr = {49,15,57,2,6,87,23};
+    arr = Sort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+
+  public static int[] Sort(int[] arr){
+    if(arr.length == 1){
+      return arr;
+    }
+
+    int mid = arr.length/2;
+
+    int[] left = Sort(Arrays.copyOfRange(arr, 0, mid));
+    int[] right = Sort(Arrays.copyOfRange(arr, mid, arr.length));
+
+
+    return merge(left,right);
+  }
+
+  private static int[] merge(int[] left, int[] right) {
+    int[] mix = new int[left.length + right.length];
+
+    int i =0;
+    int j = 0;
+    int k = 0;
+    while (i<left.length && j <right.length) {
+
+      if(left[i] < right[j]){
+        mix[k] = left[i];
+        i++;
+
+      }else{
+        mix[k] = right[j];
+        j++;
+      }
+      k++;
+    }
+
+    while (i<left.length) {
+      mix[k] = left[i];
+      i++;
+      k++;
+
+      
+    }
+    while (j<right.length)  {
+      mix[k] =right[j];
+      j++;
+      k++;
+      
+    }
+    return mix;
+  }
 }
